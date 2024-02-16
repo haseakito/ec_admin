fc92a1909f7b6ba23ecdbf8b3f33f31be10f1398
haseakito <haseakito@gmail.com>
2024-02-17 01:28:09 +0900

    :alien: Update store fetch for proper API endpoint

--- services/store.ts
+++ services/store.ts
@@ -1,45 +1,44 @@
-import { Store } from "@/types/store"
-import { auth } from "@clerk/nextjs"
+import { auth } from "@clerk/nextjs";
 
-export async function fetchStores(user_id?: string) {
-    const { getToken } = auth()
+export async function fetchStores() {
+  const { getToken } = auth();
 
-    // Base url
-    const url = user_id ? `/api/store?user_id=${user_id}` : "/api/store"
+  // GET request to fetch all stores
+  const res = await fetch(process.env.NEXT_PUBLIC_API_URL + "/stores", {
+    method: "GET",
+    mode: "cors",
+    headers: {
+      Authorization: `Bearer ${await getToken()}`,
+    },
+  });
 
-    // GET request to fetch all stores
-    const res = await fetch(process.env.NEXT_PUBLIC_API_URL + url, {
-        method: "GET",
-        mode: "cors",
-        headers: {
-            "Authorization": `Bearer ${await getToken()}`
-        },
-    })
+  if (!res.ok) {
+    throw new Error();
+  }
 
-    if (!res.ok) {
-        throw new Error()
-    }
+  // Parse json body
+  const body = await res.json();
 
-    // Parse json body
-    const body = await res.json()
-
-    return body.data
+  return body;
 }
 
 export async function fetchStore(storeId: string) {
-    const { getToken } = auth()
+  const { getToken } = auth();
 
-    // GET request to fetch a specific store with store id provided
-    const res = await fetch(process.env.NEXT_PUBLIC_API_URL + `/api/store/${storeId}`, {
-        method: "GET",
-        mode: "cors",
-        headers: {
-            "Authorization": `Bearer ${await getToken()}`
-        },
-    })
+  // GET request to fetch a specific store with store id provided
+  const res = await fetch(
+    process.env.NEXT_PUBLIC_API_URL + `/stores/${storeId}`,
+    {
+      method: "GET",
+      mode: "cors",
+      headers: {
+        Authorization: `Bearer ${await getToken()}`,
+      },
+    }
+  );
 
-    // Parse json body
-    const body = await res.json()
+  // Parse json body
+  const body = await res.json();
 
-    return body.data
+  return body;
 }
