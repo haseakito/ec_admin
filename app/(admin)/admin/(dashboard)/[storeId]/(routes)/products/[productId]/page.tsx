a240436b7b4cba468d264fa380405efdfedf7f37
haseakito <haseakito@gmail.com>
2024-02-17 01:48:31 +0900

    :sparkles: Add a specific product page

---
+++ app/(admin)/admin/(dashboard)/[storeId]/(routes)/products/[productId]/page.tsx
@@ -0,0 +1,28 @@
+import { fetchProduct } from "@/services/product";
+import { ProductForm } from "./components/product-form";
+import { ProductTitle } from "./components/title";
+import { UploadForm } from "./components/upload-form";
+
+interface ProductPageProps {
+  params: {
+    storeId: string;
+    productId: string;
+  };
+}
+
+export default async function ProductPage({ params }: ProductPageProps) {
+  // Fetch product with product id
+  const product: Product = await fetchProduct(params.productId);
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <div>
+          <ProductTitle product={product} />
+          <UploadForm product={product} />
+          <ProductForm product={product} />
+        </div>
+      </div>
+    </div>
+  );
+}
