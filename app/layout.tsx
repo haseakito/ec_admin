f1190f0dfa6ad25fe584f44a204f130a50ca196a
haseakito <haseakito@gmail.com>
2024-02-21 03:34:37 +1100

    :lipstick: Add `ConfettiProvider` to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { ThemeProvider } from "@/providers/theme-provider";
 import { ClerkProvider } from "@clerk/nextjs";
+import { ConfettiProvider } from "@/providers/confetti-provider";
 import { ToastProvider } from "@/providers/toast-provider";
 import { ModalProvider } from "@/providers/modal-provider";
 
@@ -25,6 +26,7 @@ export default function RootLayout({
             enableSystem
             disableTransitionOnChange
           >
+            <ConfettiProvider />
             <ToastProvider />
             <ModalProvider />
             {children}
