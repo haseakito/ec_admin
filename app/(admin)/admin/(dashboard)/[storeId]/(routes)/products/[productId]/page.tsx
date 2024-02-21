57bc4d75e5c40ff3b8a8aae82fad0c39071d485b
haseakito <haseakito@gmail.com>
2024-02-21 17:05:06 +1100

    :art: Add condition to render `upload-form`

--- app/(admin)/admin/(dashboard)/[storeId]/(routes)/products/[productId]/page.tsx
+++ app/(admin)/admin/(dashboard)/[storeId]/(routes)/products/[productId]/page.tsx
@@ -19,7 +19,11 @@ export default async function ProductPage({ params }: ProductPageProps) {
       <div className="flex-1 space-y-4 p-8 pt-6">
         <div>
           <ProductTitle product={product} />
-          <UploadForm product={product} />
+          {
+            product && (
+                <UploadForm product={product} />
+            )
+          }
           <ProductForm product={product} />
         </div>
       </div>
